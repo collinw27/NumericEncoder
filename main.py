fc04cf38564de92e9952b462533ed719fb8e9a34
Spencer Sturman <spencersturman@gmail.com>
2023-10-31 18:24:17 -0400

    Decode() by Spencer Sturman

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+def decode_string(encoded_password):
+    original_password = ""
+    for digit in encoded_password:
+        original_digit = str((int(digit) - 3) % 10)
+        original_password += original_digit
+    return original_password
 
 def encode_string(string):
-
     output = ''
     for char in string:
         output += str((int(char) + 3) % 10)
@@ -15,6 +20,7 @@ def main():
         print("Menu")
         print("-------------")
         print("1. Encode")
+        print("2. Decode")
         print("3. Quit")
         print()
 
@@ -23,6 +29,10 @@ def main():
         if (user_input == "1"):
             encoded_password = encode_string(input("Please enter your password to encode: "))
             print("Your password has been encoded and stored!")
+            print()
+        elif (user_input == "2"):
+            original_password = decode_string(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {original_password}.")
             print()
         elif (user_input == "3"):
             break
